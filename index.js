d105edf17ee897b45a69b243d9c928c1285e78ea
VioletaS <violistancikiene@gmail.com>
2024-07-24 10:23:50 +0300

    constructor

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-import { dog } from './js/Dog.js';
+import { Dog } from "./js/Dog.js";
 
-dog();
+const rexas = new Dog('Rexas', 'black', 3);
+console.log(rexas);
+console.log(rexas.name);
+console.log(rexas.furColor);
+console.log(rexas.age);
+
+console.log(rexas.hi());
+console.log(rexas.think());
+console.log(rexas.addNumbers(3, 2));
+console.log(rexas.addNumbers(3, -2));
+console.log(rexas.addNumbers(-3, -2));
+console.log(rexas.addNumbers(-3, 3));
+console.log(rexas.manyBones(20));
+
+
+const brisius = new Dog('Brisius', 'white', 2);
+console.log(brisius);
+
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius);
+
+console.log(brisius.birthday());
+console.log(brisius.birthday());
+console.log(brisius.birthday());
+console.log(brisius.birthday());
+console.log(brisius);
+
+const bobikas = new Dog('bobikas', 'red', 7);
+console.log(bobikas);
+console.log(bobikas.legsCount);
+console.log(bobikas.name);
+
+console.log(bobikas.hi());
+console.log(bobikas.walk(7));
+console.log(bobikas.lostLeg());
+console.log(bobikas.lostLeg());
+console.log(bobikas.lostLeg());
+console.log(bobikas.lostLeg());
+console.log(bobikas.lostLeg());
+
+console.log(bobikas.addNumbers(0, 0));
+console.log(bobikas.manyBones(5));
+console.log(bobikas.manyBones(2));
+console.log(bobikas.hasTail);
+
+// overraidini objekto reiksmes
+bobikas.legsCount = 3;
+console.log(bobikas.legsCount);
+console.log(bobikas.lostLeg());
+console.log(bobikas);
--- js/Dog.js
+++ js/Dog.js
@@ -1,6 +1,63 @@
 // jeigu .js faile yr class,
 // failo pavadinimas is didziosios raides
 
-export function dog() {
-  console.log('au, au');
+export class Dog {
+  constructor(name, furColor, age) {
+    this.name = name;
+    this.furColor = furColor;
+    this.age = age;
+    this.legsCount = 4;
+    this.hasTail = true;
+  }
+
+  // informacijos atnaujinimas ojekte
+  birthday() {
+    // this.age = this.age + 1;
+    this.age++;
+    return `Suns ${this.age} gimtadienis! 🥳🥳🥳`;
+  }
+
+  lostLeg() {
+    if (this.legsCount === 0) {
+      return 'Suo jau turi 0 (nuli) koju... daugiau nera ko prarasti 😭!';
+    }
+
+    this.legsCount--;
+    return `Suo prarado koja... 😢`;
+  }
+
+  // elementari logika, nekeicia ojekto informacijos
+
+
+
+  hi() {
+    return 'Suo sako: au au!!';
+  }
+
+  walk() {
+
+    return 'Go for a walk 🐕!!!'
+  }
+
+
+  think() {
+    return 'Suo galvoja: 🦴🦴🦴';
+  }
+
+  // parametrai pasiekiami tik metodo ribose
+  addNumbers(a, b) {
+    if (a + b < 0) {
+      return 'Suo yra liudnas... negali suskaiciuoti neigiamo kaulu kiekio :(';
+    }
+
+    if (a + b === 0) {
+      return 'Suns reakcija 👀';
+    }
+
+    return `Suo suskaiciavo: ${a} + ${b} = ${'🦴'.repeat(a + b)}.`;
+  }
+
+  manyBones(howMany) {
+    return `Many bones: ${'🦴'.repeat(howMany)}.`;
+  }
 }
